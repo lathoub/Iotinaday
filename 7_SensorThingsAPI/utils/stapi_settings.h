0e6c04b9bd0d5169a9c9163419a3480c7dc657ce
lathoub <4082369+lathoub@users.noreply.github.com>
2022-10-05 22:37:13 +0200

    Update stapi_settings.h
    v1.1 back to v1.0

--- 7_SensorThingsAPI/utils/stapi_settings.h
+++ 7_SensorThingsAPI/utils/stapi_settings.h
@@ -3,7 +3,7 @@
 const char protocol[] = "http";
 const char host[] = "stapi.aardvark.myds.me";
 const uint16_t port = 80;
-const char path[] = "/v1.1";
+const char path[] = "/v1.0";
 
 auto domain = String(host) + ":" + port + "/FROST-Server";
 auto base_url = String(protocol) + "://" + domain + path;
