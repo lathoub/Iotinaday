c1e4528ea9ad055eb80429a21aeb7e3433ece591
lathoub <4082369+lathoub@users.noreply.github.com>
2022-10-06 05:12:05 +0200

    Update 6_SendReading.ino

--- 6_SendReading/6_SendReading.ino
+++ 6_SendReading/6_SendReading.ino
@@ -1,6 +1,6 @@
 #include <ESP8266WiFi.h>
 
-float lat = 51, lng = 4, height = 4;
+float lat = 51, lng = 4, height = 4; // default value, overwrite
 
 #include "utils/logging.h"
 #include "utils/credentials.h"
