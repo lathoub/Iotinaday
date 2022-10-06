175615755d570f66f1ee6225a1c31104f0946129
lathoub <4082369+lathoub@users.noreply.github.com>
2022-10-06 05:03:31 +0200

    Update credentials.h

--- 6_SendReading/utils/credentials.h
+++ 6_SendReading/utils/credentials.h
@@ -4,4 +4,4 @@ char ssid[] = "Iotinaday";
 char pass[] = "51790615";
 
 char user[]     = "flagis";      // MySQL user login username
-char password[] = "Flagis2022_"; // MySQL user login password
+char password[] = "Rott2022_!"; // MySQL user login password
