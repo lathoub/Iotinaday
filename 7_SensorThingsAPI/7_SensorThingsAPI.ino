5dced262866566ea3392a25b6e02a663a7826a2d
lathoub <4082369+lathoub@users.noreply.github.com>
2022-10-07 15:39:31 +0200

    updated for used sensors

--- 7_SensorThingsAPI/7_SensorThingsAPI.ino
+++ 7_SensorThingsAPI/7_SensorThingsAPI.ino
@@ -14,18 +14,12 @@ float lat = 51, lng = 4;
 // forward declaration, so it can be used in readSensors
 void transmitValue(const int datastreamId, const JSONVar& observation);
 
-// Uncomment the line below, corresponding with the used sensors
 //#include "ADXL345.h"
-//#include "AGS02MA.h"
-//#include "AM2320.h"
-//#include "BMP085.h"
-//#include "BMP180.h"
 //#include "HCSR04.h"
+//#include "AM2320.h"
+//#include "MAX4466.h"
 //#include "HMC5883.h"
 //#include "HX711.h"
-#include "keyboard.h"
-//#include "MAX4466.h"
-//#include "MB060.h"
 
 void setup()
 {
